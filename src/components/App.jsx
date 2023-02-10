4521b415e37755b858922fd1a26d95265d421211
Mariia Akymenko <dafnarianna@gmail.com>
2023-02-10 15:50:33 +0200

    fix document name

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import { Title } from './Title/Title';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
-import { Main } from './App';
+import { Main } from './App.styled';
 
 export class App extends Component {
   state = {
